d0be45186452f4eb3d58f0134456aa0bd3a0e081
A-lineed <aline.edvania.franca@outlook.com>
2025-04-21 10:54:22 -0300

    Salvando a sessão do usuário

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,17 +1,32 @@
 Cypress.Commands.add('login', (
-    user = Cypress.env('user_name'),
-    password = Cypress.env('user_password'),
+  user = Cypress.env('user_name'),
+  password = Cypress.env('user_password'),
+  { cacheSession = true } = {},
 ) => {
-    const login = () => {
-        cy.visit('/users/sign_in')
+  const login = () => {
+    cy.visit('/users/sign_in')
 
-        cy.get("[data-qa-selector='login_field']").type(user)
-        cy.get("[data-qa-selector='password_field']").type(password, { log: false })
-        cy.get("[data-qa-selector='sign_in_button']").click()
+    cy.get("[data-qa-selector='login_field']").type(user)
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+    cy.get("[data-qa-selector='sign_in_button']").click()
+  }
 
-    }
+  const validate = () => {
+    cy.visit('/')
+    cy.location('pathname', { timeout: 1000 })
+      .should('not.eq', '/users/sign_in')
+  }
 
+  const options = {
+    cacheAcrossSpecs: true,
+    validate,
+  }
+
+  if (cacheSession) {
+    cy.session(user, login, options)
+  } else {
     login()
+  }
 })
 
 Cypress.Commands.add('Logout', () => {
@@ -29,4 +44,3 @@ Cypress.Commands.add('gui_createProject', project => {
     cy.contains('Create project').click()
   })
 
-  
